c90f7ccd1b669f62eed685d57d550d007291610d
Matthew Mason <m.mason29272@gmail.com>
2022-03-10 02:56:32 +0000

    changed string representation of Player class

--- Player.py
+++ Player.py
@@ -5,7 +5,7 @@ class Player:
         self.player_score = 0
     
     def __repr__(self):
-        return self.name + "\'s hand consists of: " + " ".join(str(card) + "," for card in self.player_hand) + " for a total of: " + str(self.get_score())
+        return "your hand consists of the " + " ".join(str(card) + "," for card in self.player_hand) + " for a total of: " + str(self.get_score())
 
     def get_score(self):
         return self.player_score
